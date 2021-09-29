7fe623afd9d007071a600ceed0d0eec2d5725fa0
Theodoros Ntakouris <zarkopafilis@gmail.com>
2021-09-29 15:00:52 +0300

    halfway done with functional parsing

--- src/contracts/request/events/publish_event_request.rs
+++ src/contracts/request/events/publish_event_request.rs
@@ -1,29 +1,25 @@
 use std::option::{Option};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-enum ValidPayloadType {
-    String,
-    F32,
-    I32,
-    Bool
+#[derive(Deserialize, Debug)]
+pub struct EventPayload<T>{
+    pub key: String,
+    pub tag: Option<String>,
+    pub value: Option<T>
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct EventPayload{
-    key: String,
-    tag: Option<String>,
-    value: Option<ValidPayloadType>
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum EventValue {
+    String(EventPayload<String>),
+    Float(EventPayload<f32>),
+    Int(EventPayload<i32>),
+    Bool(EventPayload<bool>)
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct PublishSingleValueEventRequest  {
-    source_id: String,
-    payload: EventPayload
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct PublishMultiValueEventRequest {
-    source_id: String,
-    payloads: Option<EventPayload>
+#[derive(Deserialize, Debug)]
+pub struct PublishSingleValueEventRequest {
+    pub source_id: String,
+    #[serde(flatten)]
+    pub manifest: EventValue
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,57 @@
 #[macro_use] extern crate rocket;
 use rocket::serde::json::Json;
+use rocket::http::Status;
 
 mod contracts;
-use contracts::request::events::publish_event_request::PublishSingleValueEventRequest;
+use contracts::request::events::publish_event_request::{PublishSingleValueEventRequest, EventValue};
 
 // #[get("/")]
 // fn index() -> &'static str {
 //     "Hello, world!"
 // }
 
-#[post("/publish_event", format="json", data="<request>")]
-pub fn publish_event(request: Json<PublishSingleValueEventRequest>) -> &'static str {
+#[post("/publish_event", format="application/json", data="<request>")]
+pub fn publish_event(request: Json<PublishSingleValueEventRequest>) -> Status {
+    let event = request.0;
     
-    "OK"
+    // validate user 
+    // validate source_id, key, tag
+
+    let evt_val: Option = match event.manifest {
+        EventValue::String(x) => x.value,
+        EventValue::Float(x) => x.value,
+        EventValue::Int(x) => x.value,
+        EventValue::Bool(x) => x.value
+    };
+
+    match evt_val {
+        Some(x) => println!("Event value: {:?}", x),
+        None => println!("No event value provided")
+    }
+
+    println!("{:?}", event.manifest);
+    match event.manifest {
+        EventValue::String(x) => {
+            println!("String payload: {:?}", x.value);
+        }
+
+        EventValue::Float(x) => {
+            println!("Float payload: {:?}", x.value);
+        }
+
+        EventValue::Int(x) => {
+            println!("Int payload: {:?}", x.value);
+        }
+        
+        EventValue::Bool(x) => {
+            println!("Bool payload: {:?}", x.value);
+        }
+    }
+
+    Status::Accepted
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/publish_event", routes![publish_event])
+    rocket::build().mount("/", routes![publish_event])
 }
